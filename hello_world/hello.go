9992df5b090ba1fc9a7b6a4323075814326b6ba4
Réda Maizate <reda.maizate@affluences.com>
2024-01-24 19:03:08 +0100

    finished hello world chapter

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello("Réda"))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	}
+	return
 }
