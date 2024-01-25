2a019b3562c2f6afb9a31a846eb640a56c2b837b
Réda Maizate <reda.maizate@affluences.com>
2024-01-25 01:47:00 +0100

    finished pointers and errors chapter

---
+++ pointers_and_errors/wallet.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+// Using the * to access the value of w and have impact in the struct. If we don't use the *, we'll have a copy of w.
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if w.balance < amount {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
